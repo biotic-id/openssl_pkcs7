d41c977afc424355746a71cb8db3111050493897
Roman Yerin <r.yerin@640kb.co.uk>
2024-11-11 13:11:07 +0100

    fix header

--- openssl_pkcs7.h
+++ openssl_pkcs7.h
@@ -6,6 +6,6 @@
 #include <stdlib.h>
 
 int verify_pkcs7(void *ca, size_t ca_len, void *signature, size_t signature_len, void *data, size_t data_len);
-char * pkcs7_attr(void *signature, size_t signature_len, int attr);
+char * pkcs7_attr(const void *signature, const int signature_len, const int attr);
 
 #endif //OPENSSL_PKCS_OPENSSL_PKCS7_H
